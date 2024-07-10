7f626b4287a82cd392c431d345149233089b3a73
NNikoGG <geekgaming222@gmail.com>
2024-07-11 00:07:33 +0530

    Add branch

--- modbus-live-monitor.py
+++ modbus-live-monitor.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import QTimer, Qt
 from pymodbus.client import ModbusTcpClient
 from pymodbus.exceptions import ModbusIOException
 
+
 class ModbusMonitorGUI(QMainWindow):
     def __init__(self):
         super().__init__()
